34381cc343b150f7e7ec908529439989f6815e5e
Ilyasse <95250342+ilyassealdidi@users.noreply.github.com>
2024-04-05 03:21:21 +0000

    updates

--- bonus/src/main_bonus.c
+++ bonus/src/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ialdidi <ialdidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 14:45:21 by ialdidi           #+#    #+#             */
-/*   Updated: 2024/03/31 12:54:43 by ialdidi          ###   ########.fr       */
+/*   Updated: 2024/04/05 03:13:07 by ialdidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int ac, char **av)
 	t_object	obj;
 
 	if (ac < 2)
-		return (ft_printf("Invalid number of arguments"), EXIT_FAILURE);
+		return (0);
 	ft_memset(&obj, 0, sizeof(obj));
 	if (!stack_init(&obj, av + 1))
 	{
@@ -28,5 +28,5 @@ int	main(int ac, char **av)
 		ft_printf("OK\n");
 	else
 		ft_printf("KO\n");
-	return (EXIT_SUCCESS);
+	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ialdidi <ialdidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 11:41:22 by ialdidi           #+#    #+#             */
-/*   Updated: 2024/03/31 20:24:08 by ialdidi          ###   ########.fr       */
+/*   Updated: 2024/04/05 03:19:17 by ialdidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int	main(int ac, char **av)
 	t_object	obj;
 
 	if (ac < 2)
-		return (ft_printf("Invalid number of arguments"), EXIT_FAILURE);
-	obj = (t_object){};
+		return (0);
+	ft_memset(&obj, 0, sizeof(obj));
 	if (!stack_init(&obj, av + 1))
 	{
 		ft_putstr_fd("Error\n", 2);
@@ -26,5 +26,5 @@ int	main(int ac, char **av)
 	}
 	sort(&obj);
 	ft_lstclear(&obj.stack_a.list, free);
-	return (EXIT_SUCCESS);
+	return (0);
 }
